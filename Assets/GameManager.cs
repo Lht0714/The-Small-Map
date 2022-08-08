bb5ce021cfcc199f185a77bea35fee07bd4b3dfd
Lht714 <2698789535@qq.com>
2022-08-08 09:18:46 +0800

    雷达小地图Demo（位置同步）

---
+++ Assets/GameManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject player;
+    
+    void Start()
+    {
+        WorldSingel.ins.player = new Obj_MainPlayer();
+        WorldSingel.ins.player.Go = player;
+    }
+
+    void Update()
+    {
+        WorldSingel.ins.player.Update();
+    }
+}
---
+++ Assets/ObjBase/Obj_MainPlayer.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obj_MainPlayer : Obj
+{
+    void Start()
+    {
+
+    }
+
+    public void Update()
+    {
+        if (Go == null)
+        {
+            return;
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            Go.transform.position += Go.transform.forward * 5 * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            Go.transform.position += Go.transform.forward * 5 * -Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            Go.transform.Rotate(Vector3.up * 40 * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            Go.transform.Rotate(Vector3.up * -40 * Time.deltaTime);
+        }
+    }
+}
